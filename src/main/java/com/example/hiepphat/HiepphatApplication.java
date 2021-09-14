c978dae162b91c015ae0dd9446df266b5a667382
hungnvse <hunguyen1499@gmail.com>
2021-09-14 13:53:21 +0700

    add config

--- src/main/java/com/example/hiepphat/HiepphatApplication.java
+++ src/main/java/com/example/hiepphat/HiepphatApplication.java
@@ -1,6 +1,8 @@
 package com.example.hiepphat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,10 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 
 @SpringBootApplication
-public class HiepphatApplication {
+public class HiepphatApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HiepphatApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HiepphatApplication.class);
 	}
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
