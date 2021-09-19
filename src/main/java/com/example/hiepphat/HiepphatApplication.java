0e4167baf78c6a0882191fc4d393d3914c9097b3
phath <phathiepchung3110@gmail.com>
2021-09-19 09:59:17 +0700

    19/9

--- src/main/java/com/example/hiepphat/HiepphatApplication.java
+++ src/main/java/com/example/hiepphat/HiepphatApplication.java
@@ -25,7 +25,7 @@ public class HiepphatApplication extends SpringBootServletInitializer {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*")
+				registry.addMapping("/**")
 						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 						.allowedHeaders("*");
 			}
