ac3cf7579be1f4081dccc8335d3a51c0a7a66749
phath <phathiepchung3110@gmail.com>
2021-11-01 19:43:23 +0700

    1/11 new ne hoho

--- src/main/java/com/example/hiepphat/repositories/LikeRecipeRepository.java
+++ src/main/java/com/example/hiepphat/repositories/LikeRecipeRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface LikeRecipeRepository extends JpaRepository<LikeRecipe,Long> {
-    @Query(value = "SELECT TOP 5 recipe_id FROM Likes_Recipe GROUP BY recipe_id ORDER BY COUNT (user_id) DESC ",nativeQuery = true)
+    @Query(value = "SELECT TOP 5 l.recipe_id FROM Likes_Recipe l,Recipes r where l.recipe_id=r.recipe_id and r.status=2 and r.is_private='false' GROUP BY l.recipe_id ORDER BY COUNT (l.user_id) DESC\n ",nativeQuery = true)
     List<Integer> findbestRecipe();
     List<LikeRecipe>findByRecipe_RecipeID(long id);
     LikeRecipe findByRecipe_RecipeIDAndUser_UserID(long recipeID,int userID);
